5f4a7ac98d46c6037cb37df82279b7fe8f16c399
Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
2024-12-14 12:41:48 -0400

    Add scripts to update files more easily, Add `apache_ajspec` argument

---
+++ update/update.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+BASEDIR=`dirname -- "$0"` || exit $?
+BASEDIR=`realpath -- "${BASEDIR}"` || exit $?
+
+. "${BASEDIR}/update.conf"
+
+set -xe
+set -o pipefail
+
+cat -- "${BASEDIR}/Makejail.template" |\
+    sed -Ee "s/%%TAG1%%/${TAG1}/g" > "${BASEDIR}/../Makejail"
+
+cat -- "${BASEDIR}/README.md.template" |\
+    sed -E \
+        -e "s/%%TAG1%%/${TAG1}/g" \
+        -e "s/%%TAG2%%/${TAG2}/g" \
+        -e "s/%%PHP1%%/${PHP1}/g" \
+        -e "s/%%PHP2%%/${PHP2}/g" \
+        -e "s/%%PHP3%%/${PHP3}/g" > "${BASEDIR}/../README.md"
